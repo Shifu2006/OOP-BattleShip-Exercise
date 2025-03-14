5dfd2ab7bf894156cf5ff98afd853f2962278122
Sina <160500733+Shifu2006@users.noreply.github.com>
2025-03-14 15:13:17 +0330

    First step

--- Answers/40330112132/Board.java
+++ Answers/40330112132/Board.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Board{
+    Scanner input = new Scanner(System.in);
+    Scanner inputint = new Scanner(System.in);
+
+    private char[][] grid;
+    private int size;
     
+    public Board(int size) {
+        this.size = size;
+        this.grid = new char[size][size];
+    }
+    
+    public int getSize() {
+        return size;
+    }
+
 }
--- Answers/40330112132/Game.java
+++ Answers/40330112132/Game.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Game{
+
+    static Scanner input = new Scanner(System.in);
+    static Scanner input2 = new Scanner(System.in);
+
     public static void main(String[] args) {
         
     }
+
+    public int askSize(){
+        System.out.print("enter size of boards(6 - 26): ");
+        int n = input2.nextInt();
+        if(n < 6 || n > 26){
+            System.out.println("out of range...");
+            return askSize();
+        }
+        return n;
+    }
 }
+
