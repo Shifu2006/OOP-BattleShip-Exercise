2430bba72323166d63c62ea3b3f76e8a6dacdd59
Sina <160500733+Shifu2006@users.noreply.github.com>
2025-03-16 02:11:05 +0330

    Refactor AI class: streamline hitOrMiss method and improve random targeting logic

--- Answers/40330112132/AI.java
+++ Answers/40330112132/AI.java
@@ -35,8 +35,6 @@ public class AI extends Player {
                                 }
                                 n++;
                             }
-                            randhorizontal = rand.nextBoolean();
-                            isPlus = rand.nextBoolean();
                         }
                         else{
                             n = -1;
@@ -51,8 +49,78 @@ public class AI extends Player {
                                 }
                                 n--;
                             }
-                            randhorizontal = rand.nextBoolean();
-                            isPlus = rand.nextBoolean();
+                        }
+                        randhorizontal = rand.nextBoolean();
+                        isPlus = rand.nextBoolean();
+                    }
+                    else{
+                        if(isPlus){
+                            n = 1;
+                            while(true){
+                                if(row + n >= trackingGrid.length || trackingGrid[row + n][col] == 'O'){
+                                    break;
+                                }
+                                if(row + n < trackingGrid.length && trackingGrid[row + n][col] == '~'){
+                                    hitOrMiss(opponentGrid, trackingGrid, row + n, col);
+                                    this.horizontal = false;
+                                    return;
+                                }
+                                n++;
+                            }
+                        }
+                        else{
+                            n = -1;
+                            while(true){
+                                if(row + n < 0 || trackingGrid[row + n][col] == 'O'){
+                                    break;
+                                }
+                                if(row + n > 0 && trackingGrid[row + n][col] == '~'){
+                                    hitOrMiss(opponentGrid, trackingGrid, row + n, col);
+                                    this.horizontal = false;
+                                    return;
+                                }
+                                n--;
+                            }
+                        }
+                        randhorizontal = rand.nextBoolean();
+                        isPlus = rand.nextBoolean();
+                    }
+                }
+            }
+            else{
+                int n;
+                boolean isPlus = rand.nextBoolean();
+                while(true){
+                    if(this.horizontal){
+                        if(isPlus){
+                            n = 1;
+                            while(true){
+                                if(col + n >= trackingGrid.length || trackingGrid[row][col + n] == 'O'){
+                                    break;
+                                }
+                                if(col + n < trackingGrid.length && trackingGrid[row][col + n] == '~'){
+                                    hitOrMiss(opponentGrid, trackingGrid, row, col + n);
+                                    this.horizontal = true;
+                                    return;
+                                }
+                                n++;
+                            }
+                            isPlus = !isPlus;
+                        }
+                        else{
+                            n = -1;
+                            while(true){
+                                if(col + n < 0 || trackingGrid[row][col + n] == 'O'){
+                                    break;
+                                }
+                                if(col + n > 0 && trackingGrid[row][col + n] == '~'){
+                                    hitOrMiss(opponentGrid, trackingGrid, row, col + n);
+                                    this.horizontal = true;
+                                    return;
+                                }
+                                n--;
+                            }
+                            isPlus = !isPlus;
                         }
                     }
                     else{
@@ -69,8 +137,7 @@ public class AI extends Player {
                                 }
                                 n++;
                             }
-                            randhorizontal = rand.nextBoolean();
-                            isPlus = rand.nextBoolean();
+                            isPlus = !isPlus;
                         }
                         else{
                             n = -1;
@@ -85,110 +152,41 @@ public class AI extends Player {
                                 }
                                 n--;
                             }
-                            randhorizontal = rand.nextBoolean();
-                            isPlus = rand.nextBoolean();
+                            isPlus = !isPlus;
                         }
-                    }
-                }
-            }
-            else{
-                int n;
-                boolean isPlus = rand.nextBoolean();
-                if(horizontal){
-                    if(isPlus){
-                        n = 1;
-                        while(true){
-                            if(col + n >= trackingGrid.length || trackingGrid[row][col + n] == 'O'){
-                                break;
-                            }
-                            if(col + n < trackingGrid.length && trackingGrid[row][col + n] == '~'){
-                                hitOrMiss(opponentGrid, trackingGrid, row, col + n);
-                                this.horizontal = true;
-                                return;
-                            }
-                            n++;
-                        }
-                        isPlus = !isPlus;
-                    }
-                    else{
-                        n = -1;
-                        while(true){
-                            if(col + n < 0 || trackingGrid[row][col + n] == 'O'){
-                                break;
-                            }
-                            if(col + n > 0 && trackingGrid[row][col + n] == '~'){
-                                hitOrMiss(opponentGrid, trackingGrid, row, col + n);
-                                this.horizontal = true;
-                                return;
-                            }
-                            n--;
-                        }
-                        isPlus = !isPlus;
-                    }
-                }
-                else{
-                    if(isPlus){
-                        n = 1;
-                        while(true){
-                            if(row + n >= trackingGrid.length || trackingGrid[row + n][col] == 'O'){
-                                break;
-                            }
-                            if(row + n < trackingGrid.length && trackingGrid[row + n][col] == '~'){
-                                hitOrMiss(opponentGrid, trackingGrid, row + n, col);
-                                this.horizontal = false;
-                                return;
-                            }
-                            n++;
-                        }
-                        isPlus = !isPlus;
-                    }
-                    else{
-                        n = -1;
-                        while(true){
-                            if(row + n < 0 || trackingGrid[row + n][col] == 'O'){
-                                break;
-                            }
-                            if(row + n > 0 && trackingGrid[row + n][col] == '~'){
-                                hitOrMiss(opponentGrid, trackingGrid, row + n, col);
-                                this.horizontal = false;
-                                return;
-                            }
-                            n--;
-                        }
-                        isPlus = !isPlus;
                     }
                 }
             }
         } else {
-            int row2 = rand.nextInt(opponentGrid.length);
-            int col2 = rand.nextInt(opponentGrid.length);
-            while(trackingGrid[row2][col2] != '~'){
-                row2 = rand.nextInt(opponentGrid.length);
-                col2 = rand.nextInt(opponentGrid.length);
+            int randrow = rand.nextInt(opponentGrid.length);
+            int randcol = rand.nextInt(opponentGrid.length);
+            while(trackingGrid[randrow][randcol] != '~'){
+                randrow = rand.nextInt(opponentGrid.length);
+                randcol = rand.nextInt(opponentGrid.length);
             }
-            hitOrMiss(opponentGrid, trackingGrid, row2, col2);
+            hitOrMiss(opponentGrid, trackingGrid, randrow, randcol);
             this.isOneHit = true;
         }
     }
 
-    public void hitOrMiss(char[][] opponentGrid, char[][] trackingGrid, int row, int col) {
-        if(opponentGrid[row][col] == 'S'){
-            trackingGrid[row][col] = 'X';
-            if(utils.shipSunk(opponentGrid, trackingGrid, row, col)){
-                System.out.println("AI chose " + (char)(col + 'A') + (row + 1) + " and sunk a ship!");
+    public void hitOrMiss(char[][] opponentGrid, char[][] trackingGrid, int row2, int col2) {
+        if(opponentGrid[row2][col2] == 'S'){
+            trackingGrid[row2][col2] = 'X';
+            if(utils.shipSunk(opponentGrid, trackingGrid, row2, col2)){
+                System.out.println("AI chose " + (char)(col2 + 'A') + (row2 + 1) + " and sunk a ship!");
                 this.lookingForShip = false;
             }
             else{
-                System.out.println("AI chose " + (char)(col + 'A') + (row + 1) + " and hit!");
+                System.out.println("AI chose " + (char)(col2 + 'A') + (row2 + 1) + " and hit!");
                 this.lookingForShip = true;
-                this.row = row;
-                this.col = col;
+                this.row = row2;
+                this.col = col2;
                 this.isOneHit = false;
             }
         }
         else{
-            trackingGrid[row][col] = 'O';
-            System.out.println("AI chose " + (char)(col + 'A') + (row + 1) + " and missed!");
+            trackingGrid[row2][col2] = 'O';
+            System.out.println("AI chose " + (char)(col2 + 'A') + (row2 + 1) + " and missed!");
         }
     }
     
